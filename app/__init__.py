bdd990f8451bc63e333386dcd88b0b854015ba26
e-mbrown <ebsonari@gmail.com>
2025-06-20 15:31:49 -0400

    Initial commit for work/hobby template. Adds route, template and minimal styling

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,7 @@
 import os
 from flask import Flask, render_template, request
 from dotenv import load_dotenv
+from . import testdata
 
 load_dotenv()
 app = Flask(__name__)
@@ -9,3 +10,7 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     return render_template('index.html', title="MLH Fellow", url=os.getenv("URL"))
+
+@app.route('/resume')
+def resume():
+    return render_template('resume.html', title="Work Experience", exp=testdata.workData, edu=testdata.schoolData, url=os.getenv("URL"))
---
+++ app/testdata.py
@@ -0,0 +1,37 @@
+workData = [
+    {
+        "name":" BingBong Inc",
+        "location":"Remote",
+        "position": [
+            {   
+                "name": "Frontend Engineer I",
+                "s_date":"jun 1990",
+                "e_date":"july 1990",
+                "desc":["Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc ultrices leo in enim porta, a imperdiet justo elementum. Etiam vulputate urna urna, sed iaculis risus cursus vitae.", "had fun", "went to bed"]
+            }, 
+            {
+                "name":"FullStack Engineer II",
+                "s_date":"july 1990",
+                "e_date":"july 2990",
+                "desc":["Api calls", "React", "Bash"]
+            },
+        ]
+    },
+    {
+        "name":" BingBing Inc",
+        "location":"Remote",
+        "position": [
+            {   
+                "name": "Frontend Engineer I",
+                "s_date":"jun 1990",
+                "e_date":"july 1990",
+                "desc":["Did stuff", "had fun", "went to bed"]
+            }, 
+        ]
+    }
+]
+
+schoolData = [
+    {"name":"Lorem", "degree":"BFA, XXX", "date":"May 1990" },
+    {"name":"Ipsum", "degree":"MFA, XXX", "date":"May 1990" }
+]
