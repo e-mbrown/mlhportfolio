eb72b718fe2b516f82245c0fbe0d4b4e3589b628
David <davidnbenjamin15@gmail.com>
2025-06-21 15:27:09 -0400

    Display leaflet map on homepage

--- app/__init__.py
+++ app/__init__.py
@@ -11,7 +11,12 @@ app = Flask(__name__)
 def index():
 # Temporary images. TODO replace with actual images.
     photos = ['David Benjamin.jpg', 'David Benjamin.jpg', 'David Benjamin.jpg', 'David Benjamin.jpg', 'David Benjamin.jpg']
-    return render_template('index.html', title="MLH Fellow", url=os.getenv("URL"), photos=photos)
+    locations = [
+        {"name": "New York", "lat": 40.7128, "lng": -74.0060, "visited_by": "David"},
+        {"name": "San Francisco", "lat": 37.7749, "lng": -122.4194, "visited_by": "Ebony"},
+        {"name": "Boston", "lat": 42.3601, "lng": -71.0589, "visited_by": "David"},
+    ]
+    return render_template('index.html', title="MLH Fellow", url=os.getenv("URL"), photos=photos, locations=locations)
 
 @app.route('/resume')
 def resume():
